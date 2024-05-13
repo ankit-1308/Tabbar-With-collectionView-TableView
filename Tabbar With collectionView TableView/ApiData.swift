3119d2cbb3e6fcc970b2911f806c58749e8300de
Ankit Maurya <mauryaankit1308@gmail.com>
2024-05-13 19:13:46 +0530

    add dataSourse and delegate in collectionView file

---
+++ Tabbar With collectionView TableView/ApiData.swift
@@ -0,0 +1,8 @@
+//
+//  ApiData.swift
+//  Tabbar With collectionView TableView
+//
+//  Created by OLX on 13/05/24.
+//
+
+import Foundation
---
+++ Tabbar With collectionView TableView/customCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  customCollectionViewCell.swift
+//  Tabbar With collectionView TableView
+//
+//  Created by OLX on 13/05/24.
+//
+
+import UIKit
+
+class customCollectionViewCell: UICollectionViewCell {
+    
+}
