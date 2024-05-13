6803028eb882609c0bd57396e7357d7e221fe8ca
Ankit Maurya <mauryaankit1308@gmail.com>
2024-05-14 01:31:14 +0530

    add flowlayout and outlet of custom classes

---
+++ Tabbar With collectionView TableView/CustomTableView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableView.swift
+//  Tabbar With collectionView TableView
+//
+//  Created by OLX on 13/05/24.
+//
+
+import UIKit
+
+class CustomTableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
